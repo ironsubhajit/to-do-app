5a8e0b4db8e1407e3f272c5e7e4f56601e409f1a
ironsubhajit <mandalsubhajit655@gmail.com>
2020-12-23 11:18:51 +0530

    Add url for delete task

--- todo/task/urls.py
+++ todo/task/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='taskList'),
-    path('task_update/<int:pk>/', update_task, name='update_task')
+    path('task_update/<int:pk>/', update_task, name='update_task'),
+    path('delete_task/<int:pk>/', delete_task, name='delete_task'),
 ]
